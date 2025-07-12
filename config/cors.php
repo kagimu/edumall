10ebcc04f235e41627a18d341e012c0cb6ffe97c
kagimu <kagimujayp01@gmail.com>
2025-07-13 00:00:06 +0300

    cors frontend

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'], // Add your API and CSRF paths
     'allowed_methods' => ['*'], // Allow all HTTP methods
-    'allowed_origins' => ['http://localhost:8080','https://edumall-admin.up.railway.app','https://edumall-uganda.netlify.app/'], // Your frontend's origin
+    'allowed_origins' => ['http://localhost:8080','https://edumall-admin.up.railway.app','https://edumall-uganda.netlify.app'], // Your frontend's origin
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'], // Allow all headers
     'exposed_headers' => [],
