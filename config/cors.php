fa3ebbea503ce298422cca45fb5a82e8a4aa2cd4
kagimu <kagimujayp01@gmail.com>
2025-07-23 15:43:25 +0300

    https all agIN

--- config/cors.php
+++ config/cors.php
@@ -3,12 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:8080', // For Vite (optional if you're using it locally)
-        'https://edumall-uganda.netlify.app', // ✅ This is your Netlify frontend
-        'https://edumall-admin.up.railway.app', // ✅ This is your Railway backend
-        
-    ],  
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
