d99880e8fcc2ebd111ed122f53897129c90cb489
kagimu <kagimujayp01@gmail.com>
2025-07-24 13:05:46 +0300

    expired error

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false, // ❗ Set to false if you don't use cookies or Laravel Sanctum
+    'supports_credentials' => true, // ❗ Set to false if you don't use cookies or Laravel Sanctum
 ];
