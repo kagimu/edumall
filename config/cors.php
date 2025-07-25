a7dfc7667a08a2b68bd87b3b75f6b190c87a4c23
kagimu <kagimujayp01@gmail.com>
2025-07-25 15:53:05 +0300

    bootstrap

--- config/cors.php
+++ config/cors.php
@@ -8,9 +8,11 @@ return [
         'http://localhost:8080', // For Vite (optional if you're using it locally)
         'https://edumall-uganda.netlify.app', // ✅ This is your Netlify frontend
     ],
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
+    'allowed_origins_patterns' => [
+        '/^https?:\/\/([a-z0-9-]+\.)?annaponsprojects\.com$/',
+    ],
+    'allowed_headers'   => ['*'],
+    'exposed_headers'   => [],
     'max_age' => 86400,
     'supports_credentials' => true, // ❗ Set to false if you don't use cookies or Laravel Sanctum
 ];
