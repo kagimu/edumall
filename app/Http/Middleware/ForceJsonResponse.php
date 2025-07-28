52973e0b518fd9b72f0ba4f6abb2674a877a2c69
tumu-10 <samueltumusiimerobin2000@gmail.com>
2025-07-28 15:50:40 +0300

    Monday five

--- app/Http/Middleware/ForceJsonResponse.php
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -4,19 +4,21 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class ForceJsonResponse
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-                $request->headers->set('Accept', 'application/json');
-        return $next($request);
+        // Force the request to expect JSON
+        $request->headers->set('Accept', 'application/json');
 
+        $response = $next($request);
+
+        // Force the response content-type to be JSON too (optional)
+        if (method_exists($response, 'header')) {
+            $response->header('Content-Type', 'application/json');
+        }
+
+        return $response;
     }
 }
