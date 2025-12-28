a40c7870be7c1391430045fb9929d9a634882bb2
kagimu <kagimujayp01@gmail.com>
2025-12-28 20:18:53 +0300

    roles-2

--- database/migrations/2025_12_28_194022_add_role_id_to_users_table.php
+++ database/migrations/2025_12_28_194022_add_role_id_to_users_table.php
@@ -10,22 +10,29 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')
-                ->default(1)
-                ->constrained('roles')
-                ->cascadeOnDelete();
-        });
-    }
+        {
+            if (!Schema::hasColumn('users', 'role_id')) {
+                Schema::table('users', function (Blueprint $table) {
+                    $table->foreignId('role_id')
+                        ->default(1)
+                        ->constrained('roles')
+                        ->cascadeOnDelete();
+                });
+            }
+        }
+
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+        {
+            if (Schema::hasColumn('users', 'role_id')) {
+                Schema::table('users', function (Blueprint $table) {
+                    $table->dropForeign(['role_id']);
+                    $table->dropColumn('role_id');
+                });
+            }
+        }
+
 };
