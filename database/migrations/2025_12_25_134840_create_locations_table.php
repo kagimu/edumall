c8527ec807c38e97e41d4eb883d224a7f4d904ff
kagimu <kagimujayp01@gmail.com>
2026-01-04 10:56:25 +0300

    supplier

--- database/migrations/2025_12_25_134840_create_locations_table.php
+++ database/migrations/2025_12_25_134840_create_locations_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (!Schema::hasTable('locations')) {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('school_id')->constrained()->cascadeOnDelete();
@@ -19,6 +20,7 @@ return new class extends Migration
             $table->unique(['school_id', 'name']); // prevent duplicates per school
         });
     }
+}
 
     /**
      * Reverse the migrations.
