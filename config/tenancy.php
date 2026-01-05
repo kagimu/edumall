f04504181f6daefeb32c9b31f8bb01d056f6b023
tumu-10 <samueltumusiimerobin2000@gmail.com>
2026-01-05 16:03:33 +0300

    Change tenancy database config to use same database with prefix instead of separate databases

--- config/tenancy.php
+++ config/tenancy.php
@@ -1,57 +1,35 @@
 <?php
 
 return [
-
     'tenant_model' => \App\Models\School::class,
 
-    'table_names' => [
+    'jobs' => 'sync',
 
-        'tenants' => 'schools',
-
-        'domains' => 'domains',
-
+    'migrations' => [
+        //
     ],
 
-    'database' => [
-
-        'central_databases' => [
-
-            'mysql' => env('DB_DATABASE', 'laravel'),
-
-        ],
-
-        'database_name' => 'school_{id}',
-
-        'template_tenant_database' => null,
-
-    ],
+    'database' => null,
 
     'cache' => [
-
-        'store' => 'redis',
-
         'tag_base' => 'tenant:',
-
     ],
 
     'filesystem' => [
-
-        'disk' => 'public',
-
         'suffix_base' => 'tenant',
-
-    ],
-
-    'jobs' => [
-
-        'queue' => 'default',
-
+        'disks' => [
+            'local',
+            'public',
+            's3',
+        ],
     ],
 
     'redis' => [
-
         'prefix_base' => 'tenant',
-
+        'prefixed_connections' => [
+            'default',
+            'cache',
+            'queue',
+        ],
     ],
-
 ];
